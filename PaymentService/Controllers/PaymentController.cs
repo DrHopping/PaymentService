d004f3d45038145fabb28a581a582dd8c61becc2
Mykyta Pedorenko <mykyta_pedorenko@epam.com>
2021-03-16 22:50:48 +0200

    Add project files.

---
+++ PaymentService/Controllers/PaymentController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using PaymentService.Models;
+
+namespace PaymentService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PaymentController : ControllerBase
+    {
+        private static int _delay = 0;
+
+        [HttpPost]
+        [Route("pay")]
+        public async Task<IActionResult> Pay([FromBody] PaymentRequestModel request)
+        {
+            try
+            {
+                await Task.Delay(_delay);
+                return request.Balance >= request.Price
+                    ? Ok(new { Result = true, Balance = request.Balance - request.Price})
+                    : Ok(new { Result = false, Balance = request.Balance});
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        [Route("delay")]
+        public IActionResult SetDelay([FromBody] int delay)
+        {
+            _delay = delay;
+            return NoContent();
+        }
+
+    }
+}
---
+++ PaymentService/Models/PaymentRequestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PaymentService.Models
+{
+    public class PaymentRequestModel
+    {
+        public int Price { get; set; }
+        public int Balance { get; set; }
+    }
+}
